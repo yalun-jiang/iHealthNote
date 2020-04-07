30dc53b7923370f7e240fc3d68d7bdaa1ae3040a
雅倫 <qscez556@gmail.com>
2020-04-07 15:48:58 +0800

    Initial Commit

---
+++ iHealthNote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iHealthNote
+//
+//  Created by KAREN_JIANG on 2020/4/7.
+//  Copyright © 2020 KAREN_JIANG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
